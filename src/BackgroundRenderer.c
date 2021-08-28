a0bddff3dad534e0a4ca5729cd34b0ff2d35e908
Jaeheon Shim <git@jaeheonshim.com>
2021-08-28 19:00:04 -0400

    Add repeating background

--- src/BackgroundRenderer.c
+++ src/BackgroundRenderer.c
@@ -12,8 +12,9 @@ static Rectangle ground1;
 static Rectangle ground2;
 
 static BgData ground1Data;
+static BgData ground2Data;
 
-static const float scrollVelocity = 300;
+static const float scrollVelocity = 100;
 
 Rectangle generateSrcRect(Rectangle rect, float pos);
 Rectangle generateDestRect(float pos, float scale);
@@ -25,16 +26,25 @@ void InitBackground(Texture2D ss) {
     ground1.y = 100;
     ground1.width = ss.width / 2.0;
     ground1.height = 26;
+
+    ground2.x = ss.width / 2.0;
+    ground2.y = 100;
+    ground2.width = ss.width / 2.0;
+    ground2.height = 26;
+
+    ground1Data.pos = 0;
+    ground2Data.pos = ground1.width;
 }
 
 void DrawBackground(float scale) {
     Vector2 origin = {0, 0};
     
     DrawTexturePro(spritesheet, generateSrcRect(ground1, ground1Data.pos), generateDestRect(ground1Data.pos, scale), origin, 0, WHITE);
+    DrawTexturePro(spritesheet, generateSrcRect(ground2, ground2Data.pos), generateDestRect(ground2Data.pos, scale), origin, 0, WHITE);
 }
 
 Rectangle generateSrcRect(Rectangle rect, float pos) {
-    rect.x = (pos >= 0 ? 0 : abs(pos));
+    rect.x += (pos >= 0 ? 0 : abs(pos));
     rect.width -= abs(pos);
     
     return rect;
@@ -48,8 +58,13 @@ Rectangle generateDestRect(float pos, float scale) {
 
 void ScrollBackground(float delta) {
     ground1Data.pos -= delta * scrollVelocity;
+    ground2Data.pos -= delta * scrollVelocity;
 
     if(ground1Data.pos + ground1.width <= 0) {
-        ground1Data.pos = ground1.width;
+        ground1Data.pos = ground2Data.pos + ground2.width;
+    }
+
+    if(ground2Data.pos + ground2.width <= 0) {
+        ground2Data.pos = ground1Data.pos + ground1.width;
     }
 }
