26e3ab1654be7f63268aefc36f64cb0ad91c7375
Jaeheon Shim <git@jaeheonshim.com>
2021-08-28 19:51:49 -0400

    Prevent double jumping

--- src/Player.c
+++ src/Player.c
@@ -1,10 +1,12 @@
 #include <raylib.h>
+#include <stdbool.h>
 
 static const float jumpVel = 1000;
 
 static float y = 0;
 static float velY = 0;
 static float accelY = -3000;
+static bool jumping = false;
 
 void DrawPlayer(float scale, float yPos) {
     DrawCircle(100 * scale, yPos + -(y * scale), 10 * scale, PURPLE);
@@ -17,9 +19,13 @@ void UpdatePlayer(float delta) {
     if(y <= 0) {
         y = 0;
         velY = 0;
+        jumping = false;
     }
 }
 
 void JumpPlayer() {
-    velY = jumpVel;
+    if(!jumping) {
+        velY = jumpVel;
+        jumping = true;
+    }
 }
